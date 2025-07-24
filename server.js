f2168dae8ccbd22a9961b40bd2c3ed9d97bdbe3a
Somesh saini <someshsaini81@gmail.com>
2025-07-24 15:59:17 +0530

    Deploy setup with custom port and db permition UPDATE

--- server.js
+++ server.js
@@ -27,3 +27,36 @@ app.get('/',(req,res) => {
 
 const PORT = process.env.PORT || 4005;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const connectDB = require('./config/db');
+// const userRoutes = require('./routes/userRoutes');
+// const DeviceRoutes = require("./routes/deviceRoutes");
+// require('dotenv').config();
+
+// const app = express();
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Middleware
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/users', userRoutes);
+// app.use('/api',DeviceRoutes);
+
+// app.get('/',(req,res) => {
+//     res.status(200).json({message : "server is working."})
+// })
+
+// const PORT = process.env.PORT || 4005;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
